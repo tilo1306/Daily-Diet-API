db78cb76cabcf79517092c2a3b3bed639873f26d
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-01 20:39:18 -0300

    feat: config initial project server.ts

---
+++ src/database/database.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+import { knex as knexDb, Knex } from 'knex'
+
+import { env } from '../env'
+
+export const config: Knex.Config = {
+  client: 'sqlite3',
+  connection: {
+    filename: env.DATABASE_URL
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations'
+  }
+}
+
+export const knex = knexDb(config)
