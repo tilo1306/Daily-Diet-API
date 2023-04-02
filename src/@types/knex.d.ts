292fdadda950ca741dc51288f0923b7fb3536ab0
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-01 22:31:24 -0300

    create type knex.d.ts

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+// eslint-disable-next-line
+import { Knex } from 'knex'
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    users: {
+      id: string
+      email: string
+      password: string
+      created_at: string
+    }
+  }
+}
