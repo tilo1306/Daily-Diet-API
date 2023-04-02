c86bc0756c3a76c95b37462f6f723b67dfe79a48
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-02 19:48:39 -0300

    refact: add interface findEmail in IUsersRepository

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -1,5 +1,7 @@
 import { ICreateUserDTO } from '../dtos/ICreateUserDTO'
+import { User } from '../infra/knex/entities/User'
 
 export interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<void>
+  findEmail(email: string): Promise<User | undefined>
 }
