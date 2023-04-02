f0d5faa5937addcfbcd8ce978a8899af3cad602a
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-02 19:49:21 -0300

    refact: add login from findEmail

---
+++ src/modules/accounts/infra/knex/repositories/UsersRepository.ts
@@ -0,0 +1,20 @@
+import { knex } from '@/database/database'
+
+import { ICreateUserDTO } from '../../../dtos/ICreateUserDTO'
+import { IUsersRepository } from '../../../repositories/IUsersRepository'
+import { User } from '../entities/User'
+
+export class UsersRepository implements IUsersRepository {
+  async create({ email, password }: ICreateUserDTO): Promise<void> {
+    await knex('users').insert({
+      email,
+      password
+    })
+  }
+  async findEmail(email: string): Promise<User | undefined> {
+    const user = (await knex('users').where({ email }).first()) as
+      | User
+      | undefined
+    return user
+  }
+}
