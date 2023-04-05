6eb31503a01d07e9fa90f2700c60188f2763254c
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-05 19:51:40 -0300

    refact: added try catch and cookies

--- src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -1,10 +1,11 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
+import { UserAlreadyExistsError } from '../errors/userAlreadyExistsError'
 import { makeCreateUserUserCase } from '../factories/makeCreateUserUseCase'
 
 export class CreateUserController {
-  handler(request: FastifyRequest, reply: FastifyReply) {
+  async handler(request: FastifyRequest, reply: FastifyReply) {
     const createTransactionBodySchema = z.object({
       email: z.string().email(),
       password: z.string().min(6)
@@ -12,10 +13,28 @@ export class CreateUserController {
 
     const { email, password } = createTransactionBodySchema.parse(request.body)
 
-    const createUserUseCase = makeCreateUserUserCase()
+    try {
+      const createUserUseCase = makeCreateUserUserCase()
 
-    createUserUseCase.execute({ email, password })
+      const newUserId = await createUserUseCase.execute({ email, password })
 
-    reply.status(201).send()
+      let { userId } = request.cookies
+
+      if (!userId) {
+        userId = newUserId
+
+        reply.cookie('userId', userId, {
+          path: '/',
+          maxAge: 1000 * 60 * 60 * 24 * 7 // 7days
+        })
+      }
+    } catch (err) {
+      if (err instanceof UserAlreadyExistsError) {
+        return reply.status(409).send({ message: err.message })
+      }
+      throw err
+    }
+
+    return reply.status(201).send()
   }
 }
