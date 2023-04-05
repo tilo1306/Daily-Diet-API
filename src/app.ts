0bb21743a80ece653645232b48ab3248993435dc
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-05 19:41:26 -0300

    refact: add fastify cookies and useRoutes

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,9 @@
+import cookie from '@fastify/cookie'
 import fastify from 'fastify'
 
+import { usersRoutes } from './routes/usersRoutes.routes'
+
 export const app = fastify()
+
+app.register(cookie)
+app.register(usersRoutes, { prefix: '/user' })
