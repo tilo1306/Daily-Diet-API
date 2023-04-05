cce08c65809423bbd6a902a0bc086d4f942100c8
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-05 19:50:25 -0300

    refact: add error class and uuid to generate id

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,21 +1,28 @@
 import { hash } from 'bcryptjs'
+import { v4 as uuidV4 } from 'uuid'
 
 import { ICreateUserDTO } from '../../dtos/ICreateUserDTO'
 import { UsersRepository } from '../../infra/knex/repositories/UsersRepository'
+import { UserAlreadyExistsError } from '../errors/userAlreadyExistsError'
 
 export class CreateUserUseCase {
   constructor(private usersRepository: UsersRepository) {}
-  async execute({ email, password }: ICreateUserDTO): Promise<void> {
+  async execute({ email, password }: ICreateUserDTO): Promise<string> {
     const emailAlreadyExists = await this.usersRepository.findEmail(email)
 
     if (emailAlreadyExists) {
-      throw new Error('Email already exists!')
+      throw new UserAlreadyExistsError()
     }
     const passwordHash = await hash(password, 6)
 
+    const id = uuidV4()
+
     await this.usersRepository.create({
+      id,
       email,
       password: passwordHash
     })
+
+    return id
   }
 }
