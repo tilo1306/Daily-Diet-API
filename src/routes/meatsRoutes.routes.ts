f0a1afe2c34707398aff96212afe4e4d527315e4
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-09 19:05:51 -0300

    feat: create route from findMeat

--- src/routes/meatsRoutes.routes.ts
+++ src/routes/meatsRoutes.routes.ts
@@ -1,12 +1,15 @@
 import { checkUserIdExists } from '@/middlewares/check-user-id-exists'
 import { CreateMeatController } from '@/modules/meat/useCase/createMeat/CreateMeatController'
+import { FindMeatController } from '@/modules/meat/useCase/findMeat/FindMeatController'
 import { ListMeatsController } from '@/modules/meat/useCase/listMeats/ListMeatsController'
 import { FastifyInstance } from 'fastify'
 
 const createMeatController = new CreateMeatController()
 const listMeatsController = new ListMeatsController()
+const findMeatUseCase = new FindMeatController()
 
 export async function meatsRoutes(app: FastifyInstance) {
   app.post('/', { preHandler: checkUserIdExists }, createMeatController.handler)
   app.get('/', { preHandler: checkUserIdExists }, listMeatsController.handler)
+  app.get('/:id', { preHandler: checkUserIdExists }, findMeatUseCase.handler)
 }
