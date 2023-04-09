1d185ce1d3fdf961349d34b0006e4d08b1c066d0
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-09 20:09:05 -0300

    feat: add updateMeat

--- src/modules/meat/repositories/IMeatsRepository.ts
+++ src/modules/meat/repositories/IMeatsRepository.ts
@@ -5,4 +5,5 @@ export interface IMeatsRepository {
   create(data: ICreateMeatDTO): Promise<Meat>
   listAllMeats(userId: string): Promise<Meat[]>
   findMeat(id: string, userId: string): Promise<Meat>
+  updateMeat(date: ICreateMeatDTO): Promise<void>
 }
