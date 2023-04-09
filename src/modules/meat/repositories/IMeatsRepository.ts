ddc942cf84a70981470a627b9302fba337219b5a
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-09 19:02:45 -0300

    feat:   add findMeat

--- src/modules/meat/repositories/IMeatsRepository.ts
+++ src/modules/meat/repositories/IMeatsRepository.ts
@@ -4,4 +4,5 @@ import { Meat } from '../infra/knex/entities/Meat'
 export interface IMeatsRepository {
   create(data: ICreateMeatDTO): Promise<Meat>
   listAllMeats(userId: string): Promise<Meat[]>
+  findMeat(id: string, userId: string): Promise<Meat>
 }
