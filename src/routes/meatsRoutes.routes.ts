052d2777c0ba71be227b81b49eb01cca7238151f
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-09 20:11:13 -0300

    feat: create route update

--- src/routes/meatsRoutes.routes.ts
+++ src/routes/meatsRoutes.routes.ts
@@ -2,14 +2,21 @@ import { checkUserIdExists } from '@/middlewares/check-user-id-exists'
 import { CreateMeatController } from '@/modules/meat/useCase/createMeat/CreateMeatController'
 import { FindMeatController } from '@/modules/meat/useCase/findMeat/FindMeatController'
 import { ListMeatsController } from '@/modules/meat/useCase/listMeats/ListMeatsController'
+import { UpdateMeatController } from '@/modules/meat/useCase/updateMeat/UpdateMeatController'
 import { FastifyInstance } from 'fastify'
 
 const createMeatController = new CreateMeatController()
 const listMeatsController = new ListMeatsController()
-const findMeatUseCase = new FindMeatController()
+const findMeatController = new FindMeatController()
+const updateMeatController = new UpdateMeatController()
 
 export async function meatsRoutes(app: FastifyInstance) {
   app.post('/', { preHandler: checkUserIdExists }, createMeatController.handler)
   app.get('/', { preHandler: checkUserIdExists }, listMeatsController.handler)
-  app.get('/:id', { preHandler: checkUserIdExists }, findMeatUseCase.handler)
+  app.get('/:id', { preHandler: checkUserIdExists }, findMeatController.handler)
+  app.put(
+    '/:id',
+    { preHandler: checkUserIdExists },
+    updateMeatController.handler
+  )
 }
